1e3877b2270253a5c7d9c9ad598291bff15d2dd7
Bruna Neves <31768250+brunaneves7@users.noreply.github.com>
2017-10-06 14:03:01 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,19 +3,19 @@
 			<p class="p">
 			<h3>Fique conectado !</h3>
 			<p class="p" style="float: right; margin-right:20%;">
-				<a href="">Termos e condições</a>
+				<a class = "a" href="">Termos e condições</a>
 			</p>
 			<p class="p">
-				<img src="gmail.png"> Email &nbsp;&nbsp;
-				<a href="">easyshoes@gmail.com</a>&nbsp;&nbsp;&nbsp;
-				<img src="twitter.png"> Twitter <a href="">	Easyshoes.com</a>
+				<img class = "img" src="gmail.png"> Email &nbsp;&nbsp;
+				<a class = "a" href="">easyshoes@gmail.com</a>&nbsp;&nbsp;&nbsp;
+				<img class = "img" src="twitter.png"> Twitter <a href="">	Easyshoes.com</a>
 			</p>
 
 			<p class="p">
-				<img src="facebook.png"> Facebook &nbsp;&nbsp;
-				<a href="">easyshoes@live.com </a>&nbsp;&nbsp;&nbsp;
-				<img src="whatsap.png"> Whatsapp 
-				<a href="">(31)813546-0849</a>
+				<img class = "img" src="facebook.png"> Facebook &nbsp;&nbsp;
+				<a class = "a" href="">easyshoes@live.com </a>&nbsp;&nbsp;&nbsp;
+				<img class = "img" src="whatsap.png"> Whatsapp 
+				<a class = "a" href="">(31)813546-0849</a>
 			</p>
 		
 			<small style="float: right;">Easyshoes.com © 2017 </small>
@@ -23,3 +23,4 @@
 		</div>
 	</footer>
 </html>
+
