57ba52f4bd882e6af2799a12ed2214e9e364573c
Jeislania <31890939+Jeislania@users.noreply.github.com>
2017-10-25 16:02:14 -0300

    Inclusão do select e atualização da página

--- pagina_funcionario.php
+++ pagina_funcionario.php
@@ -28,9 +28,6 @@
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#contact">Sobre Nós</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="login/login.php">Fazer Login</a>
-            </li>
           </ul>
         </div>
       </div>
@@ -41,7 +38,7 @@
           <div class="col-lg-8 mx-auto text-center">
             <h2 class="section-heading text-white">Bem vindo(a), (funcionário)!</h2>
             <p class="text-faded"> Este espaço é onde você, funcionário(a), terá acesso ao estoque dos produtos, assim como o setor onde ele se encontra, a quantidade disponível, marca, cor e muito mais.</p>
-            <h3 class="section-heading text-white">Tudo bem mais fácil para você!</h3><br>
+            <h3 class="section-heading text-white">Mais agilidade no seu atendimento. Aproveite!</h3><br>
           </div>
         </div>
       </div>
@@ -52,55 +49,10 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading">Produtos: </h2>
-            <h3> Ainda não há produtos cadastrados </h3>
-            <hr class="primary">
+            <?php include:"select_produtos.php"?>
           </div>
         </div>
       </div>
-
-  
-    <hr class="hr2">
-    <section id="contact">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading">Sobre Nós: </h2>
-            <hr class="primary">
-            <p>Projeto desenvolvido pelo 2º Período do Curso Informática para Internet - IPI, cadeira Projeto e Prática 1, do Professor e Orientador Alexandre Vianna. Alexandrevianna.net</p>
-            <p>Desenvolvedoras:</p>
-            <p>Bruna Rafaella De Oliveira Neves - IFPE CAMPUS IGARASSU <br> brunarafaellaneves@gmail.com </p>
-            <p>Milena Lins Basílio - IFPE CAMPUS IGARASSU <br>  milennab7@gmail.com </p>
-            <p>Jeislânia Oliveira da Silva - IFPE CAMPUS IGARASSU <br> laine.jos@gmail.com </p>
-          </div>
-        </div>
-        
-          <div class="mr-auto text-center">
-            <i class="fa fa-folder-open fa-5x sr-contact"></i>
-            <p>
-              <a href="https://github.com/Skylria/Easyshoes">Visite nosso Repositório no GitHub</a>
-            </p>
-          </div>
-        </div>
-      </div>
-            <center>
-              <div>
-                <h3 class="section-heading">Fique conectado!</h3>
-                <a href="https://www.facebook.com/">
-                  <i class="fa fa-4x fa-facebook-official" aria-hidden="true"></i>
-                </a>&nbsp;&nbsp;&nbsp;
-                <a href="https://web.whatsapp.com/">
-                  <i class="fa fa-4x fa-whatsapp" aria-hidden="true"></i>
-                </a>&nbsp;&nbsp;&nbsp;
-                <a href="https://twitter.com/login?lang=pt">
-                  <i class="fa fa-4x fa-twitter" aria-hidden="true"></i>
-                </a>&nbsp;&nbsp;&nbsp;
-                <a href="https://www.google.com/gmail/">
-                  <i class="fa fa-4x fa-google-plus-official" aria-hidden="true"></i>
-                </a><br>
-                <p></p>
-                Ou ligue para (+55)813564-1897
-              </div>
-            </center>
     </section>
     
     <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
