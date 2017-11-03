f446d0a6df345d9e93fcb5b93bded9d3ee0853b6
Bruna Neves <31768250+brunaneves7@users.noreply.github.com>
2017-11-03 15:31:03 -0200

    Add files via upload

---
+++ busca.php
@@ -0,0 +1,13 @@
+<?php
+include 'conexao.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form name="search_form" method="post" action="">
+	<input type="text" name="busca"/>
+</form>
+</body>
+</html>
