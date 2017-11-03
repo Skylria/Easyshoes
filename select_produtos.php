5ebd4b197fa128ad0faff9bf3bf78b6d1ca2ae23
Jeislania <31890939+Jeislania@users.noreply.github.com>
2017-11-03 16:17:36 -0200

    Update select_produtos.php

--- select_produtos.php
+++ select_produtos.php
@@ -1,61 +1,54 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<?php
-echo "<table style='border: solid 1px black;'>";
- echo "<tr><th>Código</th><th>Cor</th><th>Marca</th><th>Número</th><th>Setor</th><th>Quantidade</th></tr>";
-
-class TableRows extends RecursiveIteratorIterator { 
-    function __construct($it) { 
-        parent::__construct($it, self::LEAVES_ONLY); 
-    }
-
-    function current() {
-        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
-    }
-
-    function beginChildren() { 
-        echo "<tr>"; 
-    } 
-
-    function endChildren() { 
-        echo "</tr>" . "\n";
-    } 
-} 
-
-$servername = "localhost";
-$dbname   = "id3284881_cadastro";
-$username= "id3284881_ifpe";
-$password  = "123456";
-
-
-
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT codigo,cor,marca,numero,setor,quantidade FROM Cadastro_produtos"); 
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
-        echo $v;
-    }
-}
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-$conn = null;
-
-echo "</table>";
-
-   
-  
-
-?> 
-
-</body>
+    <head>
+    	<link rel="stylesheet" type="text/css" href="css/produtos.css">
+    	<title>Meus Produtos</title>
+    </head>
+    <body>
+        <table class="quadrado">
+            <tr>
+                <th>Marca </th>
+                <th>Cor </th>
+                <th>Número </th>
+                <th>Preço </th>
+                <th>Setor</th>
+                <th>Quantidade Disp.</th>
+                <th>Deletar Produto </th>
+            </tr>
+        <?php
+        $host = "localhost";
+        $usuario = "id3284881_ifpe";
+        $senha = "123456";
+        $bd = "id3284881_cadastro";
+        $strcon = mysqli_connect("$host","$usuario","$senha","$bd") or die('Erro ao conectar ao banco!');
+        $sql = "SELECT * FROM Cadastro_produtos";
+        $resultado = mysqli_query($strcon, $sql) or die('Erro ao tentar cadastrar registro');
+        $name = mysqli_query($strcon, "SELECT codigo, cor, marca, numero, setor, quantidade, preco FROM Cadastro_produtos") or die(mysqli_error($strcon));
+        $re = mysqli_fetch_array($name);
+        while($dados = mysqli_fetch_array($resultado)): ?>
+        
+        <?php
+        $codigo = $dados['codigo'];
+        $cor = $dados['cor'];
+        $marca = $dados['marca'];
+        $numero = $dados['numero'];
+        $setor = $dados['setor'];
+        $quantidade = $dados['quantidade'];
+        $preco = $dados['preco'];
+        echo "<tr>
+                <td>".$marca."</td>
+                <td>"."$cor"."</td>
+                <td>"."$numero"."</td>
+                <td>R$ "."$preco"."</td>
+                <td>".$setor."</td>
+                <td>"."$quantidade"."</td>
+                <td><a href='deletarproduto.php?id=".$codigo."'><i class='fa fa-2x fa-trash sr-icons' title='Deletar Produto'></i></a></td>
+            </tr>";
+        ?>
+        <?php
+        endwhile;
+        mysqli_close($strcon);
+        ?>
+        </table>
+    </body>
 </html>
