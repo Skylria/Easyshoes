9478d27128558e0f100bb5739e80fd25ef5e5ce1
Bruna Neves <31768250+brunaneves7@users.noreply.github.com>
2017-11-03 16:20:26 -0200

    atualização da pagina de produtos

--- produtos.php
+++ produtos.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-    <link rel="stylesheet" type="text/css" href="produtos.css">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Nossos Produtos</title>
-    <p></p>
-    <link href="css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/   css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link href="css/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
-    <link href="css/creative.min.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="index.php">EasyShoes</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="cadastro_sapato.php">Cadastrar Produto (FUNCIONÁRIOS)</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="login.php">Login (FUNCIONÁRIOS)</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<?php include 'header.php'; ?>
 
     <section class="bg-primary" id="about">
       <div class="container">
@@ -57,5 +22,5 @@
   </body>
 
 </html>
-<?php include'select_produtos.php';
+<?php include 'produtos_publicos.php';
  ?>
