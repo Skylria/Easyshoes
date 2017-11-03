2955a77ae3cf6576866c1c8aae46f93f55ebdb42
Bruna Neves <31768250+brunaneves7@users.noreply.github.com>
2017-11-03 15:56:40 -0200

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,50 +6,60 @@
   <style type="text/css">
 		p{
 			color: white;
+			font-size:13px;
+		}
+				}
+		.login {
+		    top:35% !important;
+		}
+		body {
+            background-image:url('img/header.jpg');
+            background-size:100%;
+        }
+        .a {
+            background:white;
+            text-decoration-color: black;
+        }
+        h1 {
+		    color:white !important;
 		}
 	</style>
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-	<link rel="stylesheet" type="text/css" href="css/login/style.css">
+  <link rel="stylesheet" type="text/css" href="css/login/style.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
-
-</head>
-
-<body>
-    <script >
+<script>
 function verifica(){
   senha = document.getElementById("senha").value;
-  forca = 0;
-  mostra = document.getElementById("mostra");
-  if((senha.length >= 4) && (senha.length <= 7)){
-    forca += 10;
-  }else if(senha.length>7){
-    forca += 25;
-  }
-  if(senha.match(/[a-z]+/)){
-    forca += 10;
-  }
-  if(senha.match(/[A-Z]+/)){
-    forca += 20;
-  }
-  if(senha.match(/d+/)){
-    forca += 20;
-  }
-  if(senha.match(/W+/)){
-    forca += 25;
-  }
-  return mostra_res();
+	forca = 0;
+	mostra = document.getElementById("mostra");
+	if((senha.length >= 4) && (senha.length <= 7)){
+		forca += 10;
+	}else if(senha.length>7){
+		forca += 25;
+	}
+	if(senha.match(/[a-z]+/)){
+		forca += 5;
+	}
+	if(senha.match(/[A-Z]+/)){
+		forca += 10;
+	}
+	if(senha.match(/d+/)){
+		forca += 20;
+	}
+	if(senha.match(/W+/)){
+		forca += 25;
+	}
+	return mostra_res();
 }
 function mostra_res(){
-  if(forca < 30){
-    mostra.innerHTML = '<tr><td bgcolor="red" width="'+forca+'"></td><td>Fraca </td></tr>';
-  }else if((forca >= 30) && (forca < 60)){
-    mostra.innerHTML = '<tr><td bgcolor="yellow" width="'+forca+'"></td><td>Justa </td></tr>';;
-  }else if((forca >= 60) && (forca < 85)){
-    mostra.innerHTML = '<tr><td bgcolor="blue" width="'+forca+'"></td><td>Forte </td></tr>';
-  }else{
-    mostra.innerHTML = '<tr><td bgcolor="green" width="'+forca+'"></td><td>Excelente </td></tr>';
-  }
+	if(forca < 30){
+		mostra.innerHTML = '<p>Senha Fraca </p>';
+	}else if((forca >= 30) && (forca < 60)){
+		mostra.innerHTML = '<p>Senha Justa </p>';;
+	}else if((forca >= 60) && (forca < 85)){
+		mostra.innerHTML = '<p>Forte </p>';
+	}
 }
 </script>
 </head>
@@ -58,14 +68,13 @@ function mostra_res(){
 	<div class="login">
 	<h1>Cadastro: </h1>
     <form method="post" action="bd/conexao.php">
-      <p>Nome: <input type="text" name="nome" placeholder="Nome" required="required" /></p>
-      <p>Cpf: <input type="text" name="cpf" placeholder="CPF(Apenas números)" required="required" /></p>
-      <p>Telefone: <input type="text" name="telefone" placeholder="Telefone" required="required" /></p>
-      <p>Email:<input type="email" name="email" placeholder="Email" required="required" /></p>
-      <p>Login: <input type="text" name="login" placeholder="Login" required="required" /></p>
+      <p>NOME: <input type="text" class="a" name="nome" placeholder="Ex: Ana" required="required" /></p>
+      <p>CPF: <input type="number" class="a" name="cpf" placeholder="Apenas números" required="required" /></p>
+      <p>TELEFONE: <input type="number" class="a" name="telefone" placeholder="Apenas Números (com DDD) ex: 819800000000" required="required" /></p>
+      <p>EMAIL:<input type="email" class="a" name="email" placeholder="Ex: fulano@gmail.com" required="required" /></p>
+      <p>LOGIN: <input type="text" class="a" name="login" placeholder="Ex: fulanosilva" required="required" /></p>
       <form name="teste" id="teste" onsubmit="return false">
-      Senha: <input type="password" name="senha" id="senha" onkeyup="javascript:verifica()" />
-      <table id="mostra"></table>
+      <p>SENHA: <input type="password" class="a" name="senha" id="senha" onkeyup="javascript:verifica()" placeholder="Não use senhas muito fáceis, como 123" required="required"><h1 id="mostra"></h1></p>
       <input type="submit" class="btn btn-primary btn-block btn-large" name="cadastrar" value="cadastrar">
     </form>
 </div>
