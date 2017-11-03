ee46203138e0b03991f4ee6599c10ec88cf6f9be
Bruna Neves <31768250+brunaneves7@users.noreply.github.com>
2017-11-03 16:21:20 -0200

    Update pagina_funcionario.php

--- pagina_funcionario.php
+++ pagina_funcionario.php
@@ -1,18 +1,27 @@
+<?php
+session_start();
+if (!isset($_SESSION['logado'])){
+    $_SESSION['logado'] = false;
+    $_SESSION['usuario']= "";
+}
+if ($_SESSION['logado'] == false){
+    header("location:login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
   <head>
     <meta charset="utf-8">
     <title>EasyShoes</title>
-    <link rel="stylesheet" type="text/css" href="produtos.css">
     <link href="css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/   css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     <link href="css/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
     <link href="css/creative.min.css" rel="stylesheet">
+    <style> #contact {background-color: white !important; color:black !important} </style>
   </head>
-
   <body id="page-top">
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
@@ -23,17 +32,45 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#services">Produtos</a>
+              <a class="nav-link js-scroll-trigger" href="index.php">SOBRE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php">SERVIÇOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="index.php">CONTATO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="produtos.php">NOSSOS PRODUTOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="user.php">USUÁRIOS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="pagina_funcionario.php">PÁGINA DO FUNCIONÁRIO</a></li>
+              <?php if ( $_SESSION['logado'] == true ): ?>
+            <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="aboutuser.php"?><i class="fa fa-2x fa-user-circle-o sr-icons" title="Deslogar"></i> USUÁRIO:  <?=$_SESSION['usuario']?></a></li>
+                 
+            <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="unlog.php"><i class="fa fa-2x fa-power-off sr-icons" title="Deslogar"></i></a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="compras.php"><i class="fa fa-2x fa-shopping-cart sr-icons" title="Carrinho do Cliente"></i></a></li>
+              <?php endif ?>
+              <?php if ( $_SESSION['logado'] == false ): ?>
+              <li class="nav-item"><a class="nav-link js-scroll-trigger" href="contact.php">Contato</a>
+              <li class="nav-item"><a class="nav-link js-scroll-trigger" href="login.php">Login</a>
+              <?php endif ?>
             </li>
           </ul>
         </div>
       </div>
     </nav>
+
     <section class="bg-primary" id="about">
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto text-center">
-            <h2 class="section-heading text-white">Bem vindo(a), (funcionário)!</h2>
+            <h2 class="section-heading text-white">Bem vindo(a), <?= $_SESSION['usuario']?>!</h2>
             <p class="text-faded"> Este espaço é onde você, funcionário(a), terá acesso ao estoque dos produtos, assim como o setor onde ele se encontra, a quantidade disponível, marca, cor e muito mais.</p>
             <h3 class="section-heading text-white">Mais agilidade no seu atendimento. Aproveite!</h3><br>
           </div>
@@ -46,7 +83,8 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading">Produtos: </h2>
-            <?php include'select_produtos.php'; ?>
+            <a type="button" class="btn-primary btn-lg" href="cadastro_sapato.php">Cadastrar Novo Produto</a><br>
+            <?php include 'select_produtos.php'; ?>
           </div>
         </div>
       </div>
