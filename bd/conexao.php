9400dbe51cf08b3880af9a35b059ae27e2afd84c
Bruna Neves <31768250+brunaneves7@users.noreply.github.com>
2017-11-07 15:08:46 -0300

    Update conexao.php

--- bd/conexao.php
+++ bd/conexao.php
@@ -8,14 +8,12 @@
 	} catch(PDOException $e) {
 	    echo 'ERROR: ' . $e->getMessage();
 	}
-
 	$nome = $_POST["nome"];
 	$cpf = $_POST["cpf"];
 	$telefone = $_POST["telefone"];
 	$email = $_POST["email"];
 	$login = $_POST["login"];
 	$senha = $_POST["senha"];
-
 	$sql = "INSERT INTO cadastro(U_NOME,U_CPF,U_TELEFONE,U_EMAIL,U_LOGIN,U_SENHA) 
 		VALUES(:nome, :cpf, :telefone, :email, :login, :senha)";
 	$stmt = $conn->prepare( $sql );
@@ -25,12 +23,11 @@
 	$stmt->bindParam( ':email', $email);
 	$stmt->bindParam( ':login', $login);
 	$stmt->bindParam( ':senha', $senha);
-
 	$result = $stmt->execute();
-
 	if ( ! $result ){
 	    var_dump( $stmt->errorInfo() );
 	    exit;
 	}	  
+	
 	header("location: ../login.php");
 ?>
