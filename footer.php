a44b74fc2ca539f1633821d0c2b27821092a0808
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-11-08 00:02:04 -0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,44 @@
+<br>
+<div id="footerwrap">
+	 	<div class="container">
+		 	<div class="row">
+		 		
+	 	<div class="container">
+		 	<div class="row">
+		 		
+		 		<div class="col-lg-4">
+	 			<h9 style="color: white">Redes Socias</h9>
+		 			<div class="hline-w"></div>
+		 			<p>Não perca tempo conecte-se já!</p>
+		 			<br>
+		 			<p>
+		 				<a href="#"><i class="fa fa-dribbble"></i></a>
+		 				<a href="#"><i class="fa fa-facebook"></i></a>
+		 				<a href="#"><i class="fa fa-twitter"></i></a>
+		 				<a href="#"><i class="fa fa-instagram"></i></a>
+		 				<a href="#"><i class="fa fa-tumblr"></i></a>
+		 			</p>
+		 		</div>
+		 		<div class="col-lg-4">
+	 			<h9 style="color: white">Local</h9>
+		 			<div class="hline-w"></div>
+		 			<p>
+		 				IFPE, Igarassu,<br/>
+		 				23890, Pernambuco,<br/>
+		 				Brasil.<br/>
+		 			</p>
+		 		</div>
+		 		<div class="col-lg-4">
+	 			<h9 style="color: white">Sobre</h9>
+		 			<div class="hline-w"></div>
+		 			<p>A EasyShoes é uma WPA criada com o intuito de facilitar a vida e a venda dos vendedores de sapatarias. Com o sistema é possível verificar a disponibilidade, quantidade, numeração e setor onde o produto se encontra, diminuindo o tempo de espera e garantindo um atendimento mais rápido para o cliente!</p>
+		 			
+		 		</div>
+		 	
+		 	</div>
+	 	</div>
+	 </div>
+
+
+</body>
+</html>
