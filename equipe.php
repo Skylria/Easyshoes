720bf7308fb2deec7600bc8931902d1cf9d5934f
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-11-10 12:31:55 -0200

    Create equipe.php

---
+++ equipe.php
@@ -0,0 +1,73 @@
+<?php include'header.php'?>
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Equipe de Desenvolvimento: </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+    <center>
+    <div class="container mtb">
+	 	<div class="row centered">
+		 	<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="he-wrap tpl6">
+				<img src="img/bruna.jpg" alt="">
+					<div class="he-view">
+						<div class="bg a0" data-animate="fadeIn">
+                    	</div>
+					</div>	
+				</div>
+				<h4>Bruna Neves</h4></h4>
+				<h5 class="ctitle">Programadora</h5>
+				<div class="hline"></div>
+		 	</div>
+		 	<div class="container mtb">
+	 	<div class="row centered">
+		 	<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="he-wrap tpl6">
+				<img src="img/me.jpg" alt="">
+					<div class="he-view">
+						<div class="bg a0" data-animate="fadeIn">
+                    	</div>
+					</div>	
+				</div>
+				<h4>Jeslânia Alves</h4></h4>
+				<h5 class="ctitle">Programadora</h5>
+				<div class="hline"></div>
+		 	</div>
+		 	<div class="row centered">
+		 	<div class="col-lg-3 col-md-3 col-sm-3">
+				<div class="he-wrap tpl6">
+				<img src="img/mili.jpg" alt="">
+					<div class="he-view">
+						<div class="bg a0" data-animate="fadeIn">
+                    	</div>
+					</div>	
+				</div>
+				<h4>Milena Lins </h4></h4>
+				<h5 class="ctitle">Programadora</h5>
+				<div class="hline"></div>
+		 	</div>
+		 	
+		 	</div>
+		 	</div>
+		 	</div>
+		 	</center>
+  <script src="css/vendor/jquery/jquery.min.js"></script>
+    <script src="css/vendor/popper/popper.min.js"></script>
+    <script src="css/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="css/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="css/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="css/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="js/creative.min.js"></script>
+    <script src="js/filter.js"></script>
+   
+
+ <?php include 'footer.php';?>
+
+
+
+
+     
