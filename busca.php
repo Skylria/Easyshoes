ba83becbf4eaf3371b102fdf6b93ad07f72af63b
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-11-16 16:55:52 -0300

    Update busca.php

--- busca.php
+++ busca.php
@@ -7,22 +7,29 @@
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch(PDOException $e) {
 	    echo 'ERROR: ' . $e->getMessage();
-	}	
-$sql = mysqli_query("SELECT * FROM Cadastro_produtos WHERE marca LIKE '%$busca%' OR codigo LIKE '%$busca%'");
-// query para selecionar todos os campos da tabela usuários se $busca contiver na coluna nome ou na coluna email
-// % antes e depois de $busca serve para indicar que $busca por ser apenas parte da palavra ou frase
-// $busca é a variável que foi enviada pelo nosso formulário da página anterior
-$count = mysql_num_rows($sql);
-// conta quantos registros encontrados com a nossa especificação
-if ($count == 0) {
+	}
+	    
+	$pesquisar = $_POST['busca'];
+	
+
+
+$sql = "SELECT * FROM Cadastro_produtos WHERE marca LIKE '%:pesquisar%' OR codigo LIKE '%:pesquisar%'";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(":pesquisar",$pesquisar);
+
+$result = $stmt->execute();
+
+$pesquisa = $stmt->rowCount();
+var_dump($pesquisa);
+if ($pesquisa == 0) {
     echo "Nenhum resultado!";
 } else {
     // senão
-    if ($count == 1) {
+    if ($pesquisa == 1) {
         echo "1 resultado encontrado!";
     }
     // se houver um resultado diz que existe um resultado
-    if ($count > 1) {
+    if ($pesquisa > 1) {
         echo "$count resultados encontrados!";
     }
     // se houver mais de um resultado diz quantos resultados existem
