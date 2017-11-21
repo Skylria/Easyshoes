374541dc138218b2a2ca6c1aa46cd61ef69295d4
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-11-21 19:04:18 -0200

    Create mensagem.php

---
+++ mensagem.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">	
+		<style type="text/css">
+			.f{
+				background-color:  #fcd245;
+			}
+			.ee{  font-family: Comic Sans, Comic Sans MS, cursive;
+			    font-size: 3em;
+			    font-weight: 300;
+			    line-height: 55px;
+			    margin-bottom: 25px;
+			    text-align: center;
+			    text-transform: none;
+			}
+			.zs{
+				font-family: Andale Mono, monospace;
+				font-size: 2em;
+			}
+			.ii{
+				cursor: pointer;
+			    text-align: center;
+			    background-color:#fcd245;
+			    width: 10em;
+			}
+			.enviar {
+				  -webkit-border-radius: 28;
+				  -moz-border-radius: 28;
+				  border-radius: 28px;
+				  font-family: Georgia;
+				  color: #ffffff;
+				  font-size: 26px;
+				  background: #e38532;
+				  padding: 10px 20px 10px 20px;
+				  text-decoration: none;
+}
+
+			.enviar:hover {
+				  background: #101214;
+				  background-image: -webkit-linear-gradient(top, #101214, #d99734);
+				  background-image: -moz-linear-gradient(top, #101214, #d99734);
+				  background-image: -ms-linear-gradient(top, #101214, #d99734);
+				  background-image: -o-linear-gradient(top, #101214, #d99734);
+				  background-image: linear-gradient(to bottom, #101214, #d99734);
+				  text-decoration: none;
+			}
+
+		</style>
+	</head>
+		    <body>
+		    	<center>
+			    	<p class="ii">
+	      				<i class="chevron-goodies chevron fa fa-chevron-down"></i>
+	    			</p>
+				    	<div class="f">
+				    		<br><br>
+							<p class="ee">Envie-nos uma mensagem!</p><br> <br>
+							<h3 class="zs">Digite sua mensagem no formulário abaixo:</h3><br> <br>
+							<form action="post.php" method="POST">
+								<textarea cols=120" rows="10" placeholder="*Sua Mensagem"></textarea><br> <br>
+								<input type="submit" name="Enviar" class="enviar">
+							</form>
+						</div>
+			    </center>
+			</body>
+</html>
