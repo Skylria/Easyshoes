ae923a4553bcbed6bb70c1800dbab690cb555731
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-11-24 13:21:41 -0300

    Update busca.php

--- busca.php
+++ busca.php
@@ -1,31 +1,15 @@
-
 <?php  
-if(isset($_POST['palavra-busca'])){
-	$busca = $_POST['palavra-busca'];
+  $dbname = "id3087752_cadastro";
+  $usuario="id3087752_ifpe";
+  $senha = "123456";
+  try {
+      $conn = new PDO("mysql:host=localhost;dbname=$dbname", $usuario, $senha);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+      echo 'ERROR: ' . $e->getMessage();
+  }
 
-$sql = "SELECT * FROM Cadastro_produtos WHERE marca LIKE '%$busca%'";
-	}
-	else{
-		$sql = "SELECT * FROM Cadastro_produtos ";
-}
-
-	$dbname = "id3284881_cadastro";
-    $usuario="id3284881_ifpe";
-    $senha = "123456";
-	try {
-    $result = $conexao->prepare($sql);
-	$result-> execute();
-	$totalRegistros = $result->rowCount();
-
-	  	$conn = new PDO("mysql:host=localhost;dbname=$dbname", $usuario, $senha);
-	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} 
-	catch(PDOException $e) {
-	    echo 'ERROR: ' . $e->getMessage();
-	}
- while ($dados = mysql_fetch_array($sql)) {
-        // enquanto houverem resultados...
-        echo "$dados[marca] $dados[codigo]<br>";
-        // exibir a coluna nome e a coluna email
-    }
-?>
+$busca = $_POST['busca'];
+// comando like com variavel 
+// retorna todos os produtos que tenham o valor da variável busca em qualquer posição
+$result = mysql_query("SELECT Cadastro_produtos FROM * WHERE marca like '%".$busca."%' ");
