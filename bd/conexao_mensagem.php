63a60e5c0b3aba528720e73e37d62b08f1a87310
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-01 11:44:51 -0200

    Create conexao_mensagem.php

---
+++ bd/conexao_mensagem.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+$id = $_GET['id'];
+date_default_timezone_set('America/Sao_Paulo');
+$data = date("d/m/Y"); 
+$hora = date("H:i:s");
+$usuario = $_SESSION['usuario'];
+$texto = $_POST['texto'];
+
+	$host = "localhost";
+    $dbname = "id3284797_login";
+    $usuario="id3284797_easyshoes";
+    $senha = "123456";
+    try {
+      	$conn = new PDO("mysql:host=localhost;dbname=$dbname", $usuario, $senha);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+	
+	$sql = "INSERT INTO mensagem(data, hora, usuario,texto) VALUES(?, ?, ?)";
+	$stmt = $conn->prepare( $sql );
+	$stmt->bindParam(1, $data );
+	$stmt->bindParam(2, $hora );
+	$stmt->bindParam(3,$usuario );
+	$stmt->bindParam(4, $texto);
+
+	$result = $stmt->execute();
+
+	if ( ! $result ){
+	    var_dump( $stmt->errorInfo() );
+	    exit;
+	}
+	
+header("location: ../mensagem_concluida.php");
+?>
