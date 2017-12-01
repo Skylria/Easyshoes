6578f27769e8fbe0ba7b794b68a25db601222b6c
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-01 11:47:05 -0200

    Create select_mensagem.php

---
+++ select_mensagem.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+      <link rel="stylesheet" type="text/css" href="css/produtos.css">
+        <table class="quadrado">
+            <tr>
+                <th>Id</th>
+                <th>Data</th>
+                <th>Hora</th>
+                <th>Usuário</th>
+                <th>Texto</th>
+               
+            </tr>
+        <?php
+        $host = "localhost";
+        $usuario = "id3284797_easyshoes";
+        $senha = "123456";
+        $bd = "id3284797_login";
+        $strcon = mysqli_connect("$host","$usuario","$senha","$bd") or die('Erro ao conectar ao banco!');
+        $sql = "SELECT * FROM mensagem";
+        $resultado = mysqli_query($strcon, $sql) or die('Erro ao tentar cadastrar registro');
+        $name = mysqli_query($strcon, "SELECT * FROM mensagem") or die(mysqli_error($strcon));
+         
+       
+        $data = $dados['data'];
+        $hora = $dados['hora'];
+        $usuario = $dados['usuario'];
+        $texto = $dados['texto'];
+        
+        echo "<tr>
+               
+                <td>".$data."</td>
+                <td>"."$hora"."</td>
+                <td>"."$usuario"."</td>
+                <td>"."$texto"."</td>
+                
+                
+            </tr>";
+       ?>
+        
+        </table>
+    </body>
+</html>
+
+
+ 
