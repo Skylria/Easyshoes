bd7622c4f57dd08f16dc55c30be7eeec6a6e8c91
Jeislania <31890939+Jeislania@users.noreply.github.com>
2017-12-03 15:18:32 -0300

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
   <h1>LOGIN</h1>
     <form method="post" action="bd/conexao_login.php">
       <p class="principal">Usuário: <input type="text"  class="form-control" name="usuario" placeholder="Ex: Ana" required="required" /></p>
-      <p class="principal">Senha: <input type="password" class="form-control" name="senha" placeholder="Sua senha é confidencial. Não diga a ninguém." required="required" /> </p>
+      <p class="principal">Senha: <input type="password" class="form-control" name="senha" placeholder="Sua senha é confidencial. Não compartilhe. required="required" /> </p>
         <button type="submit" class="btn btn-primary btn-block btn-large">FAZER LOGIN</button>
     </form><br>
     <p class="principal">NÃO É CADASTRADO(A)?</p>
