15caf8a478151ef3e83a95c7e9ee4867425cf84a
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-14 16:21:30 -0300

    Update select_mensagem.php

--- select_mensagem.php
+++ select_mensagem.php
@@ -1,15 +1,17 @@
 
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+    
       <link rel="stylesheet" type="text/css" href="css/produtos.css">
+      </head>
         <table class="quadrado">
             <tr>
-                <th>Id</th>
                 <th>Data</th>
                 <th>Hora</th>
                 <th>Usuário</th>
                 <th>Texto</th>
-               
+                
             </tr>
         <?php
         $host = "localhost";
@@ -20,8 +22,7 @@
         $sql = "SELECT * FROM mensagem";
         $resultado = mysqli_query($strcon, $sql) or die('Erro ao tentar cadastrar registro');
         $name = mysqli_query($strcon, "SELECT * FROM mensagem") or die(mysqli_error($strcon));
-         
-       
+        while($dados = mysqli_fetch_array($resultado)){
         $data = $dados['data'];
         $hora = $dados['hora'];
         $usuario = $dados['usuario'];
@@ -36,6 +37,7 @@
                 
                 
             </tr>";
+        }
        ?>
         
         </table>
