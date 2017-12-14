d7698254d83cf1c9d197e6d747d3ec3adc169927
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-14 16:21:59 -0300

    Update select2.php

--- select2.php
+++ select2.php
@@ -6,16 +6,17 @@
                 <th>Nome</th>
                 <th>Telefone</th>
                 <th>Email</th>
+                <th>Cargo</th>
             </tr>
         <?php
-        $host = "localhost";
-        $usuario = "root";
-        $senha = "";
-        $bd = "id3284881_cadastro";
+            $host = "localhost";
+        $usuario = "id3284797_easyshoes";
+        $senha = "123456";
+        $bd = "id3284797_login";
         $strcon = mysqli_connect("$host","$usuario","$senha","$bd") or die('Erro ao conectar ao banco!');
         $sql = "SELECT * FROM cadastro";
         $resultado = mysqli_query($strcon, $sql) or die('Erro ao tentar cadastrar registro');
-        $name = mysqli_query($strcon, "SELECT U_NOME,U_TELEFONE,U_EMAIL, U_Foto FROM cadastro") or die(mysqli_error($strcon));
+        $name = mysqli_query($strcon, "SELECT U_id, U_NOME,U_TELEFONE,U_EMAIL, U_Foto, U_CARGO FROM cadastro") or die(mysqli_error($strcon));
         while($dados = mysqli_fetch_array($resultado)): ?>
         
         <?php
@@ -23,12 +24,15 @@
         $nome = $dados['U_NOME'];
         $telefone = $dados['U_TELEFONE'];
         $email = $dados['U_EMAIL'];
+        $cargo = $dados['U_CARGO'];
+        $id = $dados['U_id'];
         echo "<tr>
                 
                 <td><img src='uploads/".$foto."' width='200px' alt='perfil'></td>
                 <td>".$nome."</td>
                 <td>"."$telefone"."</td>
                 <td>"."$email"."</td>
+                <td>"."$cargo"."<a href='bd/editadefinicao.php?id=".$id."'>edit</a></td>
                 
             </tr>";
         ?>
