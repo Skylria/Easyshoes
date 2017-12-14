b81fa6230d0ab11c63f07efcff9925161712e235
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-14 15:36:24 -0300

    Update conexao_login.php

--- bd/conexao_login.php
+++ bd/conexao_login.php
@@ -1,47 +1,32 @@
 <?php
 session_start();
-$_SESSION['usuario']="";
+$_SESSION['usuario'] = "";
 $_SESSION['logado']=false;
 $host = "localhost";
-$dbname = "id3284881_cadastro";
-$usuario="root";
-$senha = "";
-$conexao = mysqli_connect($host, $usuario, $senha) or die(mysqli_error());
-mysqli_select_db($conexao, $dbname) or die(mysqli_error());
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Autenticação</title>
-	<script type="text/javascript">
-		function ltrue(){
-			window.location=' ../pagina_funcionario.php';
-		}
-		function lfalse(){
-			window.location=' ../login.php';
-		}
-	</script>
-</head>
-<body>
-
-<?php  
+$username="id3284797_easyshoes";
+$password = "123456";
+$conn = new PDO('mysql:host=localhost;dbname=id3284797_login', $username, $password);
 $login = $_POST['usuario'];
 $senha = $_POST['senha'];
-$sql = mysqli_query($conexao,"SELECT * FROM cadastro WHERE U_LOGIN = '$login' and U_SENHA = '$senha'") or die(mysql_error('Login ou senha errados'));
-$row = mysqli_num_rows($sql);
-if($row > 0){
-	$_SESSION['login'] = $login;
-	$_SESSION['senha'] = $senha;
-	$_SESSION['logado'] = true;
-	$_SESSION['usuario']=$login;
-	echo "<script>ltrue()</script>";
-
+$stmt = $conn->prepare('SELECT * FROM cadastro WHERE U_LOGIN = ? AND U_SENHA = ?');
+$stmt->bindParam(1,$login);
+$stmt->bindParam(2,$senha);
+$stmt->execute();
+$dados = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($dados == NULL){
+    header('location:../login.php');
 }
-else{
-	echo "<script>lfalse()</script>";
-}
+else {
+        $_SESSION['logado']= true;
+        $_SESSION['cargo'] = $dados['U_CARGO'];
+        $_SESSION['usuario'] = $dados['U_LOGIN'];
+        if($dados['U_CARGO'] == "adm"){
+            header('location:../pageadm.php');
+        }
+        else{
+            header('location:../pagina_funcionario.php');
+        }
+}        
 ?>
-
 </body>
 </html>
