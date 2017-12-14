de94205677fe611963fa5ed1d7e15ee3d646134b
Jeislania <31890939+Jeislania@users.noreply.github.com>
2017-12-14 16:31:37 -0300

    Update javascripts.php

--- javascripts.php
+++ javascripts.php
@@ -1,5 +1,5 @@
 
-    <script src="js/vendor/jquery/jquery.min.js"></script>
+     <script src="js/vendor/jquery/jquery.min.js"></script>
     <script src="js/vendor/popper/popper.min.js"></script>
     <script src="js/vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="js/vendor/jquery-easing/jquery.easing.min.js"></script>
