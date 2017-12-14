3c6b08ef952f6140584741aa5daad7b5331ccac0
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-14 15:35:54 -0300

    Create conexao_cadastro.php

---
+++ bd/conexao_cadastro.php
@@ -0,0 +1,34 @@
+<?php  
+	$dbname = "id3284797_login";
+	$usuario="id3284797_easyshoes";
+	$senha = "123456";
+	$conn = new PDO('mysql:host=localhost;dbname=id3284797_login', $usuario, $senha);
+	$nome = htmlspecialchars($_POST['nome']);
+	$cpf =  htmlspecialchars($_POST['cpf']);
+	$telefone =  htmlspecialchars($_POST['telefone']);
+	$email =  htmlspecialchars($_POST['email']);
+	$senha =  htmlspecialchars($_POST['senha']);
+	$usuario =  htmlspecialchars($_POST['usuario']);
+	$data =  htmlspecialchars($_POST['data']);
+	$cargo = "NÃO DEFINIDO";
+
+  $sql ="INSERT INTO cadastro( U_NOME, U_CPF, U_TELEFONE, U_EMAIL, U_NASCIMENTO, U_LOGIN, U_SENHA, U_CARGO)
+  VALUES (?,?,?,?,?,?,?,?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindParam( 1, $nome);
+	$stmt->bindParam( 2, $cpf);
+	$stmt->bindParam( 3, $telefone);
+	$stmt->bindParam( 4, $email);
+	$stmt->bindParam( 5, $data);
+	$stmt->bindParam( 6, $usuario);
+	$stmt->bindParam( 7, $senha);
+	$stmt->bindParam( 8, $cargo);
+
+	$result = $stmt->execute();
+
+	if ( ! $result ){
+	    var_dump( $stmt->errorInfo() );
+	    exit;
+	}
+	header("location:../login.php");
+?>
