346ad6786fee2d62786b6591bac7e7fe114978a8
Jeislania <31890939+Jeislania@users.noreply.github.com>
2017-12-14 16:20:30 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,31 +1,48 @@
+<?php include 'exts/testalogado.php';?>
 <!DOCTYPE html>
 <html >
 <head>
+  <style type="text/css">
+    body {
+      background-image: url('img/header.jpg');
+      background-repeat: no-repeat;
+      background-size: 120%;
+    }
+    input {
+      background-color: white;
+    }
+    p {
+      text-align: center !important;
+    }
+    html{
+        overflow:auto !important;
+    }
+  </style>
   <meta charset="UTF-8">
-  <title>Cadastro - EasyShoes</title>
+  <title>Login - EasyShoes</title>
   
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" type="text/css" href="csslogin/style.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+  <?php include 'exts/csshrefs.php';?>
 
 </head>
 
-<body>
-  <h1 class="es">EasyShoes</h1>
+<body id="page-top">
+  <?php include 'exts/pagetopindex.php';?>
   <div class="login">
   <h1>Cadastro: </h1>
-    <form method="post" action="conexao_cadastro.php">
-      <input type="text" name="nome" placeholder="Nome" required="required" />
-      <input type="number" name="cpf" placeholder="CPF(Apenas números)" required="required" />
-      <input type="text" name="usuario" placeholder="Usuario" required="required" />
-      <input type="email" name="email" placeholder="Email" required="required" />
-      <input type="date" name="data" placeholder="Data de Nascimento" required="required" />
-      <input type="password" name="senha" placeholder="Senha" required="required" />
-      <input type="reset" name="apagar" placeholder="Apagar Dados" required="required" />
+    <form method="post" action="bd/conexao_cadastro.php">
+      <p class="principal">Nome e Sobrenome: </p><input type="text" class="form-control" name="nome" placeholder="Nome" required="required" />
+      <p class="principal">CPF: </p><input type="number" class="form-control" name="cpf" placeholder="CPF(Apenas números)" required="required" />
+      <p class="principal">Telefone: </p><input type="text" class="form-control" name="telefone" placeholder="Numero (com DDD) EX. 81-9830000000" required="required" />
+      <p class="principal">E-mail: </p><input type="email" class="form-control" name="email" placeholder="Email" required="required" />
+      <p class="principal">Data de Nascimento: </p><input type="date" class="form-control" name="data" placeholder="Data de Nascimento" required="required" />
+      <p class="principal">Usuario: </p><input type="text" class="form-control" name="usuario" placeholder="Usuario" required="required" />
+      <p class="principal">Senha: </p><input type="password" class="form-control" name="senha" placeholder="Senha" required="required" />
       <input type="submit" class="btn btn-primary btn-block btn-large" name="cadastrar">
     </form>
 </div>
-  <script  src="jslogin/index.js"></script>
 
 </body>
 </html>
