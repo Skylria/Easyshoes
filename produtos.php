71eb5524bb10e12a4467fb9b90900e7d7f06b91f
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-12-14 16:23:41 -0300

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -4,6 +4,7 @@
 <html lang="en">
 
   <head>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel = "shortcut icon" type = "imagem/x-icon" href = "/img/favicon.ico"/>
@@ -31,6 +32,9 @@
         </div>
       </div>
     </section>
+    <br><br>
+
+
 <?php
 include 'produtos_publicos.php';include 'exts/javascripts.php'; include 'footer.php'; ?>
 
